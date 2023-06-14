0a1e4e7188e097be58eacf7817c5a6a906f9671f
jare <125687149+jmc5678@users.noreply.github.com>
2023-06-14 08:24:47 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php
+       $nombre_entero = "JARED MISAEL CARREON CASTILLO";
+       $calle = "GUACHIN GAZANITIL";
+       $numero_casa = "679";
+       $colonia = "NUEVA TLAXCALA";
+       $municipio = "CUIDAD DE MEXICO";
+       $numero_telefono = "52 55 8421 6502";
+       echo  "Nombre completo: " . 
+         $nombre_entero . "<br>";
+       echo  "Dirección: " . $calle . " " . $numero_casa . ", " . $colonia . ", " . $municipio . "<br>";
+       echo  "Teléfono: " . $numero_telefono;
+    ?>
+
+  </body>
+</html>
---
+++ readme.md
@@ -0,0 +1,7 @@
+1. inicio
+2. mostrar nombre entero
+3. mostrar numero de casa
+4. mostrar colonia
+5. mostrar municipio
+6. mostrar numero telefono
+7. fin 
